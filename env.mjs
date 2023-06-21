dcfb8faed9369c0c109f2093414369b854fe38c2
kevin anantha (kevan) <kevin.anantha@gmail.com>
2023-06-21 21:14:11 +0800

    chore(env): add env.t3.gg

---
+++ env.mjs
@@ -0,0 +1,19 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+ 
+export const env = createEnv({
+  server: {
+    NEXTAUTH_URL: z.string().url(),
+    NEXTAUTH_SECRET: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: z.string().min(1),
+  },
+  runtimeEnv: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
+});
