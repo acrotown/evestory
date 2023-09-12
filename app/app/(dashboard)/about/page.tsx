3cacd06a90fec2165f7334a8989e0ab70152619d
kevan <kevin.anantha@gmail.com>
2023-09-13 03:02:33 +0800

    chore: add logo (#76)

---
+++ app/app/(dashboard)/about/page.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@/components/header"
+
+export default function About() {
+  return (
+    <main className="relative flex min-h-screen flex-col">
+      <Header />
+
+      <section className="pt-16">
+        <div className="container">
+          <h1>About Page</h1>
+        </div>
+      </section>
+    </main>
+  )
+}
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link"
 
+import { Logo } from "./icons/logo"
+
 export function MainNav() {
   return (
     <div className="hidden md:flex">
-      <Link href="/" className="mr-6 flex items-center font-display text-xl">
-        evestory
+      <Link
+        href="/"
+        className="mr-6 flex items-center justify-center font-display text-xl"
+      >
+        <Logo className="mr-2" />
+        <span className="relative top-[-2px]">evestory</span>
       </Link>
       <nav className="flex items-center space-x-6 text-sm font-medium">
         <Link href="/about">About</Link>
