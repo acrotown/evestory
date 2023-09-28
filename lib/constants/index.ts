fc2f3c9f2480b3c852ae82b62a3df37f81fd0d67
kevin anantha (kevan) <kevin.anantha@gmail.com>
2023-09-28 21:26:51 +0800

    fix: fix APP_DOMAIN constant

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,5 +1,3 @@
-import { match } from "ts-pattern"
-
 /*
   NOTE: Use home.localhost:3000 for HOME_DOMAIN and localhost:3000 for APP_DOMAIN
   in local development because Google OAuth doesn't allow subdomain localhost (e.g. app.localhost:3000)
@@ -24,7 +22,9 @@ export const APP_HOSTNAMES = new Set([
   "app.localhost:3000",
 ])
 
-export const APP_DOMAIN = match(process.env.NEXT_PUBLIC_VERCEL_ENV)
-  .with("production", () => "https://app.evestory.day")
-  .with("preview", () => "https://preview.evestory.day")
-  .with("development", () => "http://localhost:3000")
+export const APP_DOMAIN =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
+    ? "https://app.evestory.day"
+    : process.env.NEXT_PUBLIC_VERCEL_ENV === "preview"
+    ? "https://preview.evestory.day"
+    : "http://localhost:3000"
