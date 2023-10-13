10ec7e113a6be94063ef97b527085f6e8eb11389
kevan <kevan@kevans-MacBook-Pro.local>
2023-10-14 00:39:48 +0800

    chore: prisma + planetscale

--- app/api/auth/account-exists/route.ts
+++ app/api/auth/account-exists/route.ts
@@ -1,21 +1,21 @@
-import { eq } from "drizzle-orm"
 import { NextRequest, NextResponse } from "next/server"
 
-import { users } from "@/drizzle/schema"
-import drizzle from "@/lib/drizzle"
+import { conn } from "@/lib/planetscale"
 
 export const runtime = "edge"
 
 export async function POST(req: NextRequest) {
   const { email } = (await req.json()) as { email: string }
 
-  if (!drizzle) {
+  if (!conn) {
     return new NextResponse("Database connection failed", { status: 500 })
   }
 
-  const user = await drizzle.select().from(users).where(eq(users.email, email))
+  const user = await conn.execute("SELECT email FROM User WHERE email = ?", [
+    email,
+  ])
 
-  if (user.length > 0) {
+  if (user) {
     return new NextResponse(JSON.stringify({ exists: true }))
   }
   return new NextResponse(JSON.stringify({ exists: false }))
