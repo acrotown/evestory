48af5c673c77d78c6f0c15b5fb4400e8da38c98d
kevan <kevan@kevans-MacBook-Pro.local>
2023-10-17 22:55:18 +0800

    style: ring input

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -14,13 +14,13 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           type={type}
           className={cn(
-            "flex h-9 w-full rounded-l-md border border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            "flex h-9 w-full rounded-l-md rounded-r-none border border-r-0 border-input bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
             className,
           )}
           ref={ref}
           {...props}
         />
-        <div className="flex h-9 items-center rounded-r-md border-l-0 bg-muted px-3 text-foreground">
+        <div className="flex h-9 items-center rounded-r-md border-l-0 bg-muted px-3 font-medium text-foreground shadow-sm">
           {rightText}
         </div>
       </div>
