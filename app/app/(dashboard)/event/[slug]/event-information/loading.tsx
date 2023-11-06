6d518f838b76bf59492b594b9dc4d8428d78cdc0
kevan (kevin anantha) <kevin.anantha@gmail.com>
2023-11-06 20:00:43 +0800

    feat: event-information

---
+++ app/app/(dashboard)/event/[slug]/event-information/loading.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from "@/components/max-width-wrapper"
+
+export default function Loading() {
+  return (
+    <MaxWidthWrapper className="pt-10">
+      Loading event information...
+    </MaxWidthWrapper>
+  )
+}
