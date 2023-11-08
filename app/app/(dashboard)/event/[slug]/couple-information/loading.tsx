b574c024f18afea9f82c6468d4cfaefa26a0ff90
kevan (kevin anantha) <kevin.anantha@gmail.com>
2023-11-08 23:41:13 +0800

    chore: delete app and components folder

--- app/app/(dashboard)/event/[slug]/couple-information/loading.tsx
+++
@@ -1,9 +0,0 @@
-import MaxWidthWrapper from "@/components/max-width-wrapper"
-
-export default function Loading() {
-  return (
-    <MaxWidthWrapper className="pt-10">
-      Loading couple information...
-    </MaxWidthWrapper>
-  )
-}
